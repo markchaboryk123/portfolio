d73f6b19c543374087b50c991e5fb93c36a57a3e
Mark Chaboryk <markchabroyk@gmail.com>
2023-08-07 20:07:25 -0400

    mvp 1.o - final

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
-
-
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -16,9 +13,6 @@ const Container = styled.div`
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 `;
 
-
-
-
 const Heading = styled.h1`
   font-size: 32px;
   margin-bottom: 20px;
@@ -28,18 +22,19 @@ const Text = styled.p`
   font-size: 18px;
 `;
 
-
+const Image = styled.img`
+  max-width: 300px; /* Adjust the value to make the image smaller */
+  margin-top: 20px;
+`;
 
 function Home() {
   return (
     <Container>
       <Heading>Welcome to My Portfolio</Heading>
-      <Text>
-        Put something special below.
-      </Text>
+      <Text>On a mission of continuous personal improvement.</Text>
+      <Image src="assets\infinity.ico" alt="Continuous Improvement" />
     </Container>
   );
 }
-
 
 export default Home;
